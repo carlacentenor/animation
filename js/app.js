d8871b385d89e33514dc4e4de6681cf4f0f4d9b4
carlacentenor <carlacentenor@gmail.com>
2018-09-11 15:55:03 -0500

    varias animaciones

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const cuadrado = document.querySelector('.cuadrado');
+cuadrado.addEventListener('animationend',(event)=>{
+console.log(event.animationName);
+if(event.animationName == 'rebote'){
+    cuadrado.style.animationName = 'cuadrado escalera';
+    cuadrado.style.animationDuration = '3s';
+}
+})
