32eda0569d60349cdf69979b0840a14eb7b0aca9
carlacentenor <carlacentenor@gmail.com>
2018-09-12 14:40:12 -0500

    definiendo estilos

--- js/app.js
+++ js/app.js
@@ -1,8 +1,43 @@
-const cuadrado = document.querySelector('.cuadrado');
-cuadrado.addEventListener('animationend',(event)=>{
-console.log(event.animationName);
-if(event.animationName == 'rebote'){
-    cuadrado.style.animationName = 'cuadrado escalera';
-    cuadrado.style.animationDuration = '3s';
-}
+const pupil = document.querySelector('.pupil');
+const rota1 = document.querySelector('.letter--1');
+const rota2 = document.querySelector('.letter--2');
+const rota3 = document.querySelector('.letter--3');
+const rota4 = document.querySelector('.letter--4');
+const rota5 = document.querySelector('.letter--5');
+pupil.addEventListener('animationend', (event) => {
+  if (event.animationName == 'eye') {
+    animateRotate(rota1);
+    rota1.addEventListener('animationend', (event) => {
+      if (event.animationName == 'rota') {
+        animateRotate(rota2);
+        rota2.addEventListener('animationend', (event) => {
+          if (event.animationName == 'rota') {
+            animateRotate(rota3);
+            rota3.addEventListener('animationend', (event) => {
+              if (event.animationName == 'rota') {
+                animateRotate(rota4);
+                rota4.addEventListener('animationend', (event) => {
+                  if (event.animationName == 'rota') {
+                    animateRotate(rota5)
+                  }
+
+                });
+              }
+
+            });
+          }
+
+        });
+      }
+
+    });
+  }
+
 })
+
+const animateRotate = (element) => {
+  element.style.animationName = 'rota';
+  element.style.animationDuration = '2s';
+  element.style.animationIterationCount = '1';
+  element.style.animationTimingFunction = 'linear';
+};
